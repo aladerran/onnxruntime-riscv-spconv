8718933b1c2c95b42fc88548eb32938a505cd943
aladerran <120010046@link.cuhk.edu.cn>
2023-11-01 17:10:38 +0800

    Merge branch zxr into dev.

--- systolic_runner/spconvnet_runner/run-spike.sh
+++ systolic_runner/spconvnet_runner/run-spike.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 set -e
 
-./build.sh --config=Release --parallel
+# cd ../..
+
+# ./build.sh --config=Release --parallel
+
+# cd -
+
+# ./build.sh --config=Release --parallel
 
 {
 echo
 # Change the following line to your own ELF file
 spike --extension=gemmini pk ort_test -m unet_v2.onnx -p caffe2 -x 2 -O 0
-} >spike.log 2>&1
+} >unet_WS_spike.log 2>&1
+
+{
+echo
+# Change the following line to your own ELF file
+spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 2 -O 0
+} >resnet_WS_spike.log 2>&1
+
+{
+echo
+# Change the following line to your own ELF file
+spike --extension=gemmini pk ort_test -m unet_v2.onnx -p caffe2 -x 0 -O 0
+} >unet_CPU_spike.log 2>&1
+
+{
+echo
+# Change the following line to your own ELF file
+spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 0 -O 0
+} >resnet_CPU_spike.log 2>&1

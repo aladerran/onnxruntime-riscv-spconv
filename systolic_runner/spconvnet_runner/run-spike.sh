3e64c6a271d17da4c146b30db51aa8ab5a065294
aladerran <120010046@link.cuhk.edu.cn>
2023-11-02 11:49:08 +0800

    Replace backend buffer vector with dynamic array.

--- systolic_runner/spconvnet_runner/run-spike.sh
+++ systolic_runner/spconvnet_runner/run-spike.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 set -e
 
-# cd ../..
+cd ../..
 
-# ./build.sh --config=Release --parallel
+./build.sh --config=Release --parallel
 
-# cd -
+cd -
 
-# ./build.sh --config=Release --parallel
+./build.sh --config=Release --parallel
 
 {
-echo
 # Change the following line to your own ELF file
 spike --extension=gemmini pk ort_test -m unet_v2.onnx -p caffe2 -x 2 -O 0
 } >unet_WS_spike.log 2>&1
 
 {
-echo
 # Change the following line to your own ELF file
 spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 2 -O 0
-} >resnet_WS_spike.log 2>&1
+} >resnet_WS_spike.log 2>>&1
 
 {
-echo
 # Change the following line to your own ELF file
 spike --extension=gemmini pk ort_test -m unet_v2.onnx -p caffe2 -x 0 -O 0
 } >unet_CPU_spike.log 2>&1
 
 {
-echo
 # Change the following line to your own ELF file
 spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 0 -O 0
 } >resnet_CPU_spike.log 2>&1

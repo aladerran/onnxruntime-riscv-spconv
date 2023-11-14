7c1c0102b554ea0bf0d72bc7bf86f9beb431c351
aladerran <120010046@link.cuhk.edu.cn>
2023-11-14 19:30:41 +0800

    Fix spike script bug & Add sparse data.

--- systolic_runner/spconvnet_runner/run-spike.sh
+++ systolic_runner/spconvnet_runner/run-spike.sh
@@ -12,19 +12,23 @@ cd -
 
 ./build.sh --config=Release --parallel
 
+# rm -f *.log
+rm -f *.csv *.onnx
+cp -r data/0.1k/* .
+
 # Run tests and append output to log files
 # For each test, replace the placeholder with your ELF file path and other parameters
 {
   echo ===================== Runtime begins =====================
-  spike --extension=gemmini pk ort_test -m unet_v2.onnx -p caffe2 -x 2 -O 99
+  spike --extension=gemmini pk ort_test -m unet_v2.onnx -p caffe2 -x 1 -O 99
   echo ===================== Runtime ends =====================
-} >> unet_WS_spike.log 2>&1
+} >> unet_Gemmini_spike.log 2>&1
 
-{
-  echo ===================== Runtime begins =====================
-  spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 2 -O 99
-  echo ===================== Runtime ends =====================
-} >> resnet_WS_spike.log 2>&1
+# {
+#   echo ===================== Runtime begins =====================
+#   spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 1 -O 99
+#   echo ===================== Runtime ends =====================
+# } >> resnet_Gemmini_spike.log 2>&1
 
 # {
 #   echo ===================== Runtime begins =====================
@@ -37,3 +41,6 @@ cd -
 #   spike --extension=gemmini pk ort_test -m resnet_v2.onnx -p caffe2 -x 0 -O 99
 #   echo ===================== Runtime ends =====================
 # } >> resnet_CPU_spike.log 2>&1
+
+
+rm -f *.csv *.onnx
